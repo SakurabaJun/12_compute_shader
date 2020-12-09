0f751ae7ebccd588130a7f87cd09eaf62e0dabb7
Sakuraba_Jun <nyantan1918@gmail.com>
2020-12-09 20:15:43 +0900

    プログラムワークショップⅣ第12回提出課題

--- src/Assets/ParticleBehaviourScript.cs
+++ src/Assets/ParticleBehaviourScript.cs
@@ -24,6 +24,8 @@ public class ParticleBehaviourScript : MonoBehaviour
     ComputeShader computeShader;
     int updateKernel;
     ComputeBuffer buffer;
+    ComputeBuffer buffer_visible;
+    ComputeBuffer buffer_args;
 
     int maxParticleNum = ((1024 * 1024 + 31) / 32) * 32;
 
@@ -42,6 +44,18 @@ public class ParticleBehaviourScript : MonoBehaviour
             Marshal.SizeOf(typeof(Particle)), 
             ComputeBufferType.Default);
 
+        //可視パーティクルを登録するためのバッファ
+        buffer_visible = new ComputeBuffer(
+            maxParticleNum,
+            Marshal.SizeOf(typeof(uint)),
+            ComputeBufferType.Append);
+
+        //描画数は管理するためのバッファ
+        buffer_args = new ComputeBuffer(
+            1,
+            16,
+            ComputeBufferType.IndirectArguments);
+
         // 初期化関数の設定
         var initKernel = computeShader.FindKernel("initialize");
         computeShader.SetBuffer(initKernel, "Particles", buffer);
@@ -50,23 +64,37 @@ public class ParticleBehaviourScript : MonoBehaviour
         // 更新関数の設定
         updateKernel = computeShader.FindKernel("update");
         computeShader.SetBuffer(updateKernel, "Particles", buffer);
+        computeShader.SetBuffer(updateKernel, "Visibles", buffer_visible);
 
         // 描画用マテリアルの設定
-		material = new Material(shader);
+        material = new Material(shader);
         material.SetTexture("_MainTex", mainTexture);
         material.SetBuffer("particles", buffer);
+        material.SetBuffer("visibles", buffer_visible);
     }
 
     void OnDisable()
     {
         buffer.Release();
+        buffer_visible.Release();
+        buffer_args.Release();
     }
 
     void Update()
     {
+        int[] args_init = new int[] { 0, 1, 0, 0 };
+        buffer_args.SetData(args_init);
+        buffer_visible.SetCounterValue(0);
+
         // 運動
+        Matrix4x4 mV = Camera.main.worldToCameraMatrix;
+        Matrix4x4 mP = Camera.main.projectionMatrix;
+        Matrix4x4 mVP = mP * mV;
+        computeShader.SetMatrix("mVP", mVP);
         computeShader.SetFloat("deltaTime", Time.deltaTime);
         computeShader.Dispatch(updateKernel, maxParticleNum / 32, 1, 1);
+
+        ComputeBuffer.CopyCount(buffer_visible, buffer_args, 0);
     }
 
     /// <summary>
@@ -75,6 +103,6 @@ public class ParticleBehaviourScript : MonoBehaviour
     void OnRenderObject()
     {
         material.SetPass(0);
-        Graphics.DrawProceduralNow(MeshTopology.Points, maxParticleNum);
+        Graphics.DrawProceduralIndirectNow(MeshTopology.Points, buffer_args, 0);
     }
 }
